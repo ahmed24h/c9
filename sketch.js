5b90801fbb203d34a3c528f82fcac6f49ba942bc
ahmed24h <117548729+ahmed24h@users.noreply.github.com>
2022-11-05 19:38:48 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var box 
+
+
+
+function setup() {
+  createCanvas(400,400);
+box = createSprite(200,200,10,10)
+
+}
+
+function draw() 
+{
+  background(30);
+if (keyDown (RIGHT_ARROW)){
+  box.position.x=box.position.x+5
+}
+if (keyDown (LEFT_ARROW)){
+  box.position.x=box.position.x-5
+}
+drawSprites();
+
+
+}
+
+
+
+
